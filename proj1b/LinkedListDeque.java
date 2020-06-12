3817e6c38f714a1c9f0e41fb5f546fdb2e50d65a
onlymouse2797 <66396384+onlymouse2797@users.noreply.github.com>
2020-06-12 14:02:11 -0400

    Add files via upload

---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,124 @@
+public class LinkedListDeque<T> implements Deque<T>{
+
+    //node class
+    private class ListNode {
+        private T item;
+        private ListNode prev;
+        private ListNode next;
+        public ListNode(T i, ListNode p, ListNode n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+        public ListNode(T i) {
+            item = i;
+        }
+    }
+
+    //parameters
+    private int size;
+    private ListNode sentinel;
+
+    //Constructor
+    public LinkedListDeque() {
+        sentinel = new ListNode(null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    //Methods
+    @Override
+    public void addFirst(T item) {
+        ListNode first = new ListNode(item);
+        first.next = sentinel.next;
+        first.prev = sentinel;
+        first.next.prev = first;
+        sentinel.next = first;
+        size = size + 1;
+    }
+
+    @Override
+    public void addLast(T item) {
+        ListNode last = new ListNode(item);
+        last.next = sentinel;
+        last.prev = sentinel.prev;
+        sentinel.prev = last;
+        last.prev.next = last;
+        size = size + 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        ListNode show = sentinel.next;
+        while (show != sentinel) {
+            System.out.print(show.item + " ");
+            show = show.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        ListNode result = sentinel.next;
+        result.next.prev = sentinel;
+        sentinel.next = result.next;
+        size = size - 1;
+        return result.item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        ListNode result = sentinel.prev;
+        result.prev.next = sentinel;
+        sentinel.prev = result.prev;
+        size = size - 1;
+        return result.item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        ListNode result = sentinel;
+        for (int i = 0; i < index + 1; i = i + 1) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        ListNode result = getRecursivehelper(sentinel, index);
+        return result.item;
+    }
+
+    private ListNode getRecursivehelper(ListNode current, int index) {
+        ListNode result = current;
+        if (index == 0) {
+            result = current.next;
+        } else {
+            result = getRecursivehelper(current.next, index - 1);
+        }
+        return result;
+    }
+}
